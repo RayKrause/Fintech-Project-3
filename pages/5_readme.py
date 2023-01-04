204ab4252895166033b9ce4747c843843be2438f
Ray Krause <109116465+RayKrause@users.noreply.github.com>
2023-01-04 13:50:11 -0700

    updated readme

--- pages/5_readme.py
+++ pages/5_readme.py
@@ -18,7 +18,7 @@ st.markdown('<div style="text-align: justify;"> The application is easy to use a
 st.markdown(' ')
 
 st.markdown('## Crypto Currencies')
-st.markdown('<div style="text-align: justify;"> The initial datasets outlined for crypto currencies include some of the basic information for the latest day of trading.  The Technical Indicators section ins applicable to both the stocks and crypto landing page and includes a Bollinger Bands, Moving Average Convergence Divergence, Relative Strength Index, True Strength Index and Rate of Change.  Each graph includes a button that will direct the user to helpful information on how to read each graph and their signifigance to the overall securitys financial picture. The final data set that is included in the crypto landing page is a prior 10 day snapshot.</div>', unsafe_allow_html=True)
+st.markdown('<div style="text-align: justify;"> The initial datasets outlined for crypto currencies include some of the basic information for the latest day of trading. The Technical Indicators section is applicable to the stocks, crypto, currency and commodities landing pages and includes a Bollinger Bands, Moving Average Convergence Divergence, Relative Strength Index, True Strength Index and Rate of Change. Each graph includes a button that will direct the user to helpful information on how to read each graph and their signifigance to the overall securitys financial picture. The final data set that is included in the crypto landing page is a prior 10 day snapshot.</div>', unsafe_allow_html=True)
 st.markdown(' ')
 image = Image.open('./images/Crypto_Dataset.png')
 st.image(image)
@@ -45,6 +45,9 @@ st.markdown('<div style="text-align: justify;"> The TSI fluctuates between posit
 st.markdown(' ')
 st.markdown("#### Key ROC Takeaway's")
 st.markdown('<div style="text-align: justify;"> Rate of change (ROC) refers to how quickly something changes over time. It is thus the acceleration or deceleration of changes (i.e., the rate) and not the magnitude of individual changes themselves. In finance, rate of change is used to understand price returns and identify momentum in trends. Moving averages are used by traders to understand the rates of change in asset prices by smoothing them out. The Price Rate of Change indicator is a technical tool that measures the percentage change in price between the current price and the price a certain number of periods ago. </ div>', unsafe_allow_html=True)
+st.markdown(' ')
+st.markdown('## Currency and Commodities')
+st.markdown('<div style="text-align: justify;"> The Currency and Commodities landing pages includes all of the information as above including the Technical Indicators for Bollinger Bands, Moving Average Convergence Divergence, Relative Strength Index, True Strength Index and Rate of Change.</div>', unsafe_allow_html=True)
 st.markdown(' ')
 st.markdown('## Stocks')
 st.markdown('<div style="text-align: justify;"> The stocks landing page includes all of the information as above but also includes a section of buy & sell recomendations from multiple sources below the prior days closing information.  After the graphs and 10 day snapshot you can find the Additional Corporate Data section which shows a list of the largest institutional holders of the stock, a section on the breakdown of major shareholders, the most recent financials, balance sheet and cashflow.</div>', unsafe_allow_html=True)
